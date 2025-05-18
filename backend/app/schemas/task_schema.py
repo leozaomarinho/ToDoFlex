3366e2840a3b8a218884ff9b99659498c37dec4a
leonardo <leowalkg6@Gmail.com>
2025-05-18 17:59:08 -0300

    creating schemas to task class

--- backend/app/schemas/task_schema.py
+++ backend/app/schemas/task_schema.py
@@ -2,19 +2,24 @@ from pydantic import BaseModel
 from typing import Optional
 
 class TaskBase(BaseModel):
+    #TaskBase: contém os campos básicos (title e description).
     title: str
     description: Optional[str] = None
 
 class TaskCreate(TaskBase):
+    #TaskCreate: igual ao TaskBase, mas pode ser estendido depois.
     pass
 
 class TaskUpdate(TaskBase):
+    #TaskUpdate: inclui completed para permitir atualizações parciais.
     completed: Optional[bool] = None
 
 class Task(TaskBase):
+    #Task: usado nas respostas, inclui id e completed.
     id : int
     completed: bool
 
     class Config:
+        # orm_mode = True: permite que o Pydantic converta objetos do ORM (SQLAlchemy) em dicionários JSON.
         orm_mode = True
         # Permite que o Pydantic converta os dados do ORM para o formato esperado
