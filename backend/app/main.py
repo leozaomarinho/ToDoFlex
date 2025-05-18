b7d4547eb38b66600a44417b082cad419dd5c3d7
leonardo <leowalkg6@Gmail.com>
2025-05-17 23:03:23 -0300

    add extension on gitignore

---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.routers import task_routes
+
+app = FastAPI()
+# Include routers
+
+app.include_router(task_routes.router)
+
+
+@app.get("/")
+def read_root():
+    return{ "message": "Api do ToDoFlex rodando!"}
