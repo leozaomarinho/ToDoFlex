f66239bb8f61d1477f35e94dff1a1359bb598ec0
leonardo <leowalkg6@Gmail.com>
2025-05-18 00:19:37 -0300

    creating database connection

---
+++ backend/app/database.py
@@ -0,0 +1,19 @@
+# database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///./todoflex.db"  
+# SQLite database file
+
+# Conexao com o banco de dados
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+# Criando sessoes no banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class para os modelos
+Base = declarative_base()
