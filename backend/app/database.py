74bb38818ceecc2dd5520bca918640fe322768dd
leonardo <leowalkg6@Gmail.com>
2025-05-20 17:46:41 -0300

    try fix import on routes related on database

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,6 +2,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "sqlite:///./todoflex.db"  
 # SQLite database file
