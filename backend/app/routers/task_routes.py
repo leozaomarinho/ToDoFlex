ec12f942e46a1d02e31d8d64cca47cb8e25d6edb
leonardo <leowalkg6@Gmail.com>
2025-05-25 16:15:28 -0300

    fix routes

--- backend/app/routers/task_routes.py
+++ backend/app/routers/task_routes.py
@@ -1,20 +1,15 @@
-# app/routers/task_routes.py
-from fastapi import APIRouter, Depends,HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 
 from app.database import get_db
 from app.schemas.task_schema import Task, TaskCreate, TaskUpdate
 from app.services.task_service import (
-
-
     create_task,
     get_task,
-
     update_task,
     delete_task,
     get_task_by_id,
-
 )
 
 router = APIRouter(
@@ -23,11 +18,8 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[Task])
-def read_tasks(task_id: int = None, db: Session = Depends(get_db)):
-    tarefa = get_task(db, task_id)
-    if not tarefa:
-        raise HTTPException(status_code=404, detail="Task not found")
-    return tarefa
+def read_tasks(db: Session = Depends(get_db)):
+    return get_task(db)
 
 @router.get("/{task_id}", response_model=Task)
 def read_task(task_id: int, db: Session = Depends(get_db)):
@@ -42,17 +34,14 @@ def create_new_task(task: TaskCreate, db: Session = Depends(get_db)):
 
 @router.put("/{task_id}", response_model=Task)
 def update_existing_task(task_id: int, task: TaskUpdate, db: Session = Depends(get_db)):
-    tarefa = get_task_by_id(db, task_id)
+    tarefa = update_task(db, task_id, task)
     if not tarefa:
         raise HTTPException(status_code=404, detail="Task not found")
     return tarefa
 
 @router.delete("/{task_id}", response_model=Task)
 def delete_existing_task(task_id: int, db: Session = Depends(get_db)):
-    tarefa = get_task_by_id(db, task_id)
+    tarefa = delete_task(db, task_id)
     if not tarefa:
         raise HTTPException(status_code=404, detail="Task not found")
-    return {
-        "message": "Task deleted successfully",
-        "task": tarefa
-    }
+    return tarefa
