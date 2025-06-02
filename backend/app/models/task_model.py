23b47ae313b387691ca9dd0d1322f1aa1c158cbe
leonardo <leowalkg6@Gmail.com>
2025-06-01 21:47:35 -0300

    fix tablename tasks

--- backend/app/models/task_model.py
+++ backend/app/models/task_model.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 from app.database import Base
 
 class Task(Base):
-    _tablename_ = "tasks"
+    __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
