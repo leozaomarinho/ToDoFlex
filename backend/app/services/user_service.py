96b772bd00a9a0f2d577b22c1855d389f355a361
leonardo <leowalkg6@Gmail.com>
2025-06-13 22:57:54 -0300

    creating user service

---
+++ backend/app/services/user_service.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from app.models.user_model import User
+from app.schemas.user_schema import UserCreate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def create_user(db: Session, user: UserCreate) -> User:
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        hashed_password=get_password_hash(user.password)
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user_by_username(db: Session, username: str) -> User:
+    return db.query(User).filter(User.username == username).first()
