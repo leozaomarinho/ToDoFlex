2cf1094b18ab68f5597b4cf9dbd467cf9eb40b84
leonardo <leowalkg6@Gmail.com>
2025-06-15 21:06:58 -0300

    change configuration to fix errors

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -15,4 +15,5 @@ class UserResponse(UserBase):
     id: int
 
     class Config:
+        from_atributes = True  # Permite que o Pydantic use atributos de classe como campos
         orm_mode = True  # Permite que o Pydantic converta modelos ORM em dicionários
