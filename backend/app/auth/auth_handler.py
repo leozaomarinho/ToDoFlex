14468d20c31395c993ec214be5ff9049e1b7ae15
leonardo <leowalkg6@Gmail.com>
2025-06-17 21:41:53 -0300

    valida token

--- backend/app/auth/auth_handler.py
+++ backend/app/auth/auth_handler.py
@@ -5,6 +5,7 @@ SECRET_KEY = ""
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+#Cria o JWT com um tempo de expiração.
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
 
@@ -17,4 +18,12 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+# Verifica o token JWT e retorna o payload se for válido, ou None se não for
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
 
